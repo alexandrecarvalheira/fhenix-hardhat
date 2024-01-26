d9fe6ef062e30e918995da3c243820683229b2db
Alexandre Carvalheira <axxcar@gmail.com>
2024-01-25 23:28:21 -0300

    removes old scripts

--- scripts/deployerc20.js
+++ scripts/deployerc20.js
@@ -4,22 +4,19 @@ async function deploy() {
   const accounts = await ethers.getSigners();
   const contractOwner = accounts[0];
 
-  const name = "TestShieldedToken";
-  const symbol = "TST";
+  const name = "FHENIX";
+  const symbol = "FHE";
 
   const EncryptedERC20 = await hre.ethers.getContractFactory("EncryptedERC20");
   const encryptedERC20 = await EncryptedERC20.connect(contractOwner).deploy(name, symbol);
   await encryptedERC20.waitForDeployment();
 
-  console.log(`Counter deployed to: ${await encryptedERC20.getAddress()}`);
+  console.log(`encryptedERC20 deployed to: ${await encryptedERC20.getAddress()}`);
 }
 
-if (require.main === module) {
-  // === This is for deploying a new diamond ===
-  deploy()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-}
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
